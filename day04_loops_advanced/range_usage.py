4140b24857cabb52f573623e94c8003e7197d140
sai poshith reddy aramati <253964105+sai-poshith@users.noreply.github.com>
2026-01-29 10:31:52 +0530

    Create range_usage.py
    understanding range concepts in loops

---
+++ day04_loops_advanced/range_usage.py
@@ -0,0 +1,9 @@
+# in this case only stop
+for i in range(5):
+  print("hello")
+# in this both start and stop
+for i in range(1,5):
+  print(i)
+# in this case we included start and stop and the jumo between the two numbers also
+for i in range(1,11,2):
+  print(i)
