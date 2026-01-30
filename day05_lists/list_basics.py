b53574c3ae19be76a7e8dd85e9210b53fca31a33
sai poshith reddy aramati <253964105+sai-poshith@users.noreply.github.com>
2026-01-30 09:21:17 +0530

    basics of list and their syntax

---
+++ day05_lists/list_basics.py
@@ -0,0 +1,12 @@
+# creating a list
+numbers=[10,20,30,40,50]
+#accesing list elements 
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+#changing list values
+numbers[1]=25
+print(numbers)
+#length of a list
+print(len(numbers))
