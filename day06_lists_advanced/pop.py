1b8d2714391d1d56bcbda40bfa59e8d48949044d
sai poshith reddy aramati <253964105+sai-poshith@users.noreply.github.com>
2026-01-31 17:17:22 +0530

    using pop to remove elements from  list

---
+++ day06_lists_advanced/pop.py
@@ -0,0 +1,5 @@
+#using pop to remove the elements in a list
+numbers = [10,20,30,40]
+numbers.pop() # if its empty inside pop then it removes the last element in index
+numbers.pop(1) #we will write the index value to remove the particular element from list
+print(numbers)
