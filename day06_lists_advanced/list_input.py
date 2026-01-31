7094c6f97cd30d2c6474487ae9f0ad1c7988ffc3
sai poshith reddy aramati <253964105+sai-poshith@users.noreply.github.com>
2026-01-31 17:31:59 +0530

    list_exmaple.py
    examle with combination of conditions,lists,loop and input

---
+++ day06_lists_advanced/list_input.py
@@ -0,0 +1,18 @@
+numbers = []
+for i in range(5):
+    n=int(input())
+    numbers.append(n)
+print(numbers)
+for m in numbers30
+44
+82
+66
+:
+    if m>=40:
+        print("pass")
+    elif m>=60:
+        print("A Grade")
+    elif m>=80:
+        print("A+ Grade")
+    else:
+        print("Fail")
