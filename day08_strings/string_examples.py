51fff50a6b365a6e9dc227cc98c295b7e2998264
sai poshith reddy aramati <253964105+sai-poshith@users.noreply.github.com>
2026-02-02 09:36:58 +0530

    day08:String basic problems

---
+++ day08_strings/string_examples.py
@@ -0,0 +1,16 @@
+#Print only vowels from a string
+word="hello"
+for ch in word:
+    if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
+        print(ch)
+#Count how many characters are in a string
+count=0
+for ch in word:
+    count=count+1
+#Print first and last character of a string
+print(word[0])
+print(word[len(word)-1])
+#Create a string and print each character
+for ch in word:
+    print(ch)
+   
