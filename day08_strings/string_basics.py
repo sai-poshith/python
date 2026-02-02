7f6ca9a237aee0e257818a31850d55468b70e064
sai poshith reddy aramati <253964105+sai-poshith@users.noreply.github.com>
2026-02-02 09:27:40 +0530

    Day 8: string basics and looping
    immutable concept

---
+++ day08_strings/string_basics.py
@@ -0,0 +1,6 @@
+word="hello"
+print(len(word)) # it will give length of the string
+# strings are immutable 
+# we cant change them directly
+new_word ="Y"+word[1:] #this is the way to change the character in the string 
+print(new_word)
