1bc1c729022b23718e5013f8447a1f714379f641
sai poshith reddy aramati <253964105+sai-poshith@users.noreply.github.com>
2026-02-04 09:26:32 +0530

    most used and common string methods

---
+++ day10_strings_advanced/string_methods.py
@@ -0,0 +1,11 @@
+text = "PyThOn"
+print(text.lower())  # python 
+print(text.upper())  # PYTHON
+text = "  hello  "
+print(text.strip())  #to remove empty spaces before and after the text not in between the text
+text = "banana"
+print(text.count("a"))  # 3
+text = "hello world"
+print(text.replace("world", "python"))
+
+
