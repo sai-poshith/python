5aa91ad66c5440c73236038b3c226f9ad3352983
sai poshith reddy aramati <253964105+sai-poshith@users.noreply.github.com>
2026-02-07 23:12:05 +0530

    Create a file and write data into it.

---
+++ day13_file_handling/write_file.py
@@ -0,0 +1,11 @@
+# Write data to a file
+
+file = open("data.txt", "w")
+
+file.write("Hello Python\n")
+file.write("This is file handling\n")
+file.write("Day 13 learning")
+
+file.close()
+
+print("Data written to file")
